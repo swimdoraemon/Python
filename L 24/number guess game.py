a138319d9972cf68f6e665df6dcbe7adfdb86654
swimdoraemon <indoaradhya@gmail.com>
2025-01-18 11:50:42 +0900

    me

---
+++ L 24/number guess game.py
@@ -0,0 +1,19 @@
+import random
+n = random.randint(1,100)
+
+
+if n%2 == 0 :
+    print ("Hint :  the number is even (2,4,6,8,0)")
+else:
+    print ("Hint :  the number is odd (1,3,5,7,9)")
+
+while True:
+    guess = int(input ("guess the number between 1-100 : "))
+    if guess > n:
+        print("you nedd to guess a number lower than that")
+    elif guess < n:
+        print ("you need to guess something higher than that")
+    else:
+        print ("you got it!")
+        break
+
